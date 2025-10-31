f53e2e63490442fce2661dec1ef23b045ca110c6
WilliamAbon <96871965+WilliamAbon@users.noreply.github.com>
2025-10-31 15:18:47 +0700

    Update and rename ModulGetDisplayNameAndUsername.txt to ModulGetDisplayNameAndUsername.lua

---
+++ ModulGetDisplayNameAndUsername.lua
@@ -0,0 +1,20 @@
+local se = {}
+
+function se:GetUsername(Http)
+	local gam = game:GetService("UserService")--Pastikan Http ini dalam nya table seperti ModuleScript
+	local se = loadstring(game:HttpGet(Http))()
+	local t = {}
+	local num = 1
+	for i, v in ipairs(se) do
+		if type(v) == "string" then
+			local sd = tonumber(v)
+			table.insert(t, gam:GetUserInfosByUserIdsAsync({sd})[num].Username)
+		elseif type(v) == "number" then
+			table.insert(t, gam:GetUserInfosByUserIdsAsync({sd})[num].Username)
+		end
+		num += 1
+	end
+	return t
+end
+
+return se
