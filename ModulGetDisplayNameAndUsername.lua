53eca88b0dd1a2d5e15fb5aa154a20d8c64ddcd5
WilliamAbon <96871965+WilliamAbon@users.noreply.github.com>
2025-11-01 12:05:30 +0700

    Update ModulGetDisplayNameAndUsername.lua

--- ModulGetDisplayNameAndUsername.lua
+++ ModulGetDisplayNameAndUsername.lua
@@ -1,19 +1,31 @@
 local ye = {}
 
-function ye.ngambilUserName(Http)
-	--local stri = Http
+function ye.ngambilUsername(Http)
 	local gam = game:GetService("UserService")--Pastikan Http ini dalam nya table seperti ModuleScript
 	local se = Http
-	local t = {}
-	local num = 1
+	local t = nil
 	for i, v in ipairs(se) do
 		if type(v) == "string" then
 			local sd = tonumber(v)
-			table.insert(t, gam:GetUserInfosByUserIdsAsync({sd})[num].Username)
+			t = gam:GetUserInfosByUserIdsAsync({sd})[1].Username)
 		elseif type(v) == "number" then
-			table.insert(t, gam:GetUserInfosByUserIdsAsync({v})[num].Username)
+		t = gam:GetUserInfosByUserIdsAsync({v})[1].Username)
 		end
-		num += 1
+	end
+	return t
+end
+
+function ye.ngambilDisplayName(Http)
+	local gam = game:GetService("UserService")--Pastikan Http ini dalam nya table seperti ModuleScript
+	local se = Http
+	local t = nil
+	for i, v in ipairs(se) do
+		if type(v) == "string" then
+			local sd = tonumber(v)
+			t = gam:GetUserInfosByUserIdsAsync({sd})[1].DisplayName)
+		elseif type(v) == "number" then
+		t = gam:GetUserInfosByUserIdsAsync({v})[1].DisplayName)
+		end
 	end
 	return t
 end
